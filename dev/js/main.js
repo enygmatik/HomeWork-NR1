00798eaf7a73c3697d8a5484a368f97684c966ed
John Manoil <ionmnl@gmail.com>
2015-01-23 01:12:12 +0200

    Full work menu ( menu RC1 )

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,37 +1,52 @@
-/**
- * Created by ionmnl on 1/14/15.
- */
+$(function(){
+    var app = $(function(){
 
-$(function () {
-    var fake_upl = $('#fake_upload'),
-        upload_btn = $('#upload_btn'),
-        menu_btn = $('.menu_btn');
+        //Show selected file for upload
+        $(function () {
+            var fake_upl = $('#fake_upload'),
+                upload_btn = $('#upload_btn'),
+                menu_btn = $('.menu_btn');
+            $(fake_upl).on('click', function () {
+                $(upload_btn).click();
+            });
 
-    $(fake_upl).on('click', function () {
-        $(upload_btn).click();
+
+            $(upload_btn).on('change', function () {
+                var input = $(this),
+                    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [numFiles, label]);
+            });
+
+            $(upload_btn).on('fileselect', function (event, numFiles, label) {
+
+                var input = $(this).parents('.upload_form'),
+                    fileName = numFiles > 1 ? numFiles + ' files selected' : label;
+                if (input.length) {
+                    $('.selected-files').prepend("<div>" + "File: " + fileName + "<div/>");
+                }
+            });
+
+            $(function () {
+                menu_btn.on('click', function () {
+                    var side = $('.left_side_bar');
+                    side.toggleClass('collapsed');
+                })
+            })
+        });
+
+        $(function(){
+            // Get current url
+            // Select an a element that has the matching href and apply a class of 'active'. Also prepend a - to the content of the link
+            //var url = window.location.href;
+            //$('.menu a[href="'+url+'"]').addClass('current_page_item');
+            //
+            //console.log(url);
+
+            $('.nav li a').each(function(index) {
+                if(this.href.trim() == window.location)
+                    $(this).addClass("active");
+            });
+        });
     });
-
-
-    $(upload_btn).on('change', function () {
-        var input = $(this),
-            numFiles = input.get(0).files ? input.get(0).files.length : 1,
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label]);
-    });
-
-    $(upload_btn).on('fileselect', function (event, numFiles, label) {
-
-        var input = $(this).parents('.upload_form'),
-            fileName = numFiles > 1 ? numFiles + ' files selected' : label;
-        if (input.length) {
-            $('.selected-files').prepend("<div>" + "File: " + fileName + "<div/>");
-        }
-    });
-
-    $(function () {
-        menu_btn.on('click', function () {
-            var side = $('.left_side_bar');
-            side.toggleClass('collapsed');
-        })
-    })
 });
--- dist/blocks/nav.php
+++ dist/blocks/nav.php
@@ -1,7 +1,7 @@
 <div class="menu">
     <ul class="nav">
-        <li><a href="index" class="active nav_items">Обо мне</a></li>
-        <li><a href="portfolio" class="nav_items">Мои работы</a></li>
-        <li><a href="contact_me" class="nav_items">Связаться со мной</a></li>
+        <li class="menu_item active"><a href="index" class="active nav_items">Обо мне</a></li>
+        <li class="menu_item"><a href="portfolio" class="nav_items">Мои работы</a></li>
+        <li class="menu_item"><a href="contact_me" class="nav_items">Связаться со мной</a></li>
     </ul>
 </div>
