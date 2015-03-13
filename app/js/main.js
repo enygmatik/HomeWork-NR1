be9beeef6383b25cecc741c5e54fe2247deb22cb
John Manoil <ionmnl@gmail.com>
2015-03-13 22:16:42 +0200

    Bug fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,193 +1,100 @@
-$(function () {
-    var app = $(function () {
+(function(){
+    var app = {
+        //APLICATION START
+        initialize: function () {
+            console.log('APP Init');
 
-        var contact_Form = $('#contactForm'),
-            upload_Form = $('#add_project_form'),
-            inputs1 = contact_Form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
-            inputs2 = upload_Form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
-            email = $('#email'),
-            mail_status = $('.status_message_box');
+            this.setUpListener();
+            this.activeMenuItem();
 
-        // Check valid email format
-        $(email).on("keydown", function () {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (re.test($(this).val())) {
-                $(this).addClass('has-succes');
-            }
-            else {
-                $(this).addClass('has-error');
-            }
-        });
+            //$('input, textarea').placeholder();
 
-        // Submit Contact form
-        $(contact_Form).on('submit', function () {
-            var inputs = contact_Form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
-                validInputs = 0;
+        },
 
-                    $(inputs).tooltip('destroy');
+        //EVENT KEEPER
+        setUpListener: function() {
+            var form = $('form'),
+                closeModalBtn = $('.btn-close-modal'),
+                openMenuBtn = $('.icon-menu');
 
-                        $.each(inputs, function (index, val) {
-                            var input = $(val),
-                                val = input.val(),
-                                textError = 'Fill the input';
+                $('.btn_disable').on('click', app.clearForm);
+                form.on('keydown', '.has-error', app.removeError);
+                form.on('submit', app.contactMe);
+                closeModalBtn.on('click', app.clearForm);
+                openMenuBtn.on('click', app.openMobileMenu);
+        },
 
-                            if (val.length === 0) {
-                                $(input).addClass('has-error');
+        //SEND DATA FROM CONTACT FORM
+        contactMe: function () {
+            if(!app.validateForm()) return false;
 
-                                $(input).tooltip({
-                                    trigger : 'manual',
-                                    placement: 'auto',
-                                    title: textError
-                                }).tooltip('show');
-                            } else {
-                                validInputs++;
-                                $(input).addClass('has-succes');
-                            }
-                            console.log('In Each: ' + validInputs)
-                        });
-                        console.log('Out Each: ' + validInputs);
+            var data = $('form').serialize();
+            $.ajax({
+                type: 'POST',
+                url: './mail.php',
+                data: data
+            }).done(function(response){
+                console.log("sending");
+                    if (response === 'Your message was sent') {
+                        $('.status_message_box').html('<p class="mail_sent">' + response + '</p>');
+                        app.clearForm();
+                    }
+                    if (response === 'Wrong captcha' ) {
+                        $('.status_message_box').html('<p class="mail_sent_error">' + response + '</p>');
+                        $('.input_captcha').addClass('has-error');
+                    } else {
+                        $('.input_captcha').removeClass('has-error');
+                    }
+            });
+            return false;
+        },
 
-            // IF all inputs is VALID go AJAX
-            $(function(){
+        //VALIDATE FORM ON CLIENT SIDE
+        validateForm: function () {
+            console.log('Check form');
+            var elements = $('form').find('input, textarea'),
+                valid = true;
 
-                if ( validInputs === 4) {
-                    var data = contact_Form.serialize();
+            $.each(elements, function(index, val) {
+                var element = $(val),
+                    val = element.val();
 
-                        $.ajax({
-                            type: "POST",
-                            data: data,
-                            url: './mail.php'
-                        }).done(function(){
-                            $(contact_Form).trigger("reset");
-                            $(inputs2).removeClass('has-succes has-error');
-                            $(mail_status).html("<span class='mail_sent'> Your mail was sent.</span>");
-
-
-                        }).fail(function(){
-                            $(mail_status).html("<span class='mail_sent_error'> Problem on server, please try later</span>");
-                        })
-
-                } else {
-                    //alert("stop!");
+                if(val.length === 0){
+                    element.addClass('has-error');
+                    valid = false;
                 }
             });
-            return false
-        });
+            return valid;
+        },
 
-        // Submit Upload form
-        $(upload_Form).on('submit', function () {
-            var inputs = upload_Form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
-                validInputs = 0;
+        //CLEAR FORM
+        clearForm: function() {
+            console.log('Clear form');
+            var form = $('form');
+            form.find('.has-error').removeClass('has-error');
+            form.find('input, textarea').val('');
+        },
 
-            $(inputs).tooltip('destroy');
+        //REMOVE ERROR INPUT STYLE ON TYPE
+        removeError: function() {
+            $(this).removeClass('has-error');
+        },
 
-            $.each(inputs, function (index, val) {
-                var input = $(val),
-                    val = input.val(),
-                    textError = 'Fill the input';
-
-                if (val.length === 0) {
-                    $(input).addClass('has-error');
-
-                    $(input).tooltip({
-                        trigger : 'manual',
-                        placement: 'left',
-                        title: textError
-                    }).tooltip('show');
-                } else {
-                    validInputs++;
-                    $(input).addClass('has-succes');
-                }
-                console.log('In Each: ' + validInputs)
-            });
-            console.log('Out Each: ' + validInputs);
-
-            // IF all inputs is VALID go AJAX
-            $(function(){
-
-            });
-            return false
-
-        });
-
-        //Remove tooltip from invalid input's when typing on CONTACT FORM
-        $(inputs1).on('keydown', function (){
-            $(this).tooltip('destroy');
-            if ($(this).val === 0) {
-                $(this).removeClass('has-succes')
-                $(this).addClass('has-error');
-            } else {
-                $(this).removeClass('has-error');
-                $(this).addClass('has-succes');
-            }
-        });
-
-        //Remove tooltip from invalid input's when typing on UPLOAD FORM
-        $(inputs2).on('keydown change', function (){
-            $(this).tooltip('destroy');
-            if ($(this).val === 0) {
-                $(this).removeClass('has-succes')
-                $(this).addClass('has-error');
-            } else {
-                $(this).removeClass('has-error');
-                $(this).addClass('has-succes');
-            }
-        });
-
-        //Show selected file for upload
-        $(function () {
-            var fake_upl = $('#fake_upload'), upload_btn = $('#upload_btn');
-
-            $(fake_upl).on('click', function () {
-                $(upload_btn).click();
-            });
-
-            $(upload_btn).on('change', function () {
-                var input = $(this),
-                    numFiles = input.get(0).files ? input.get(0).files.length : 1,
-                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-                input.trigger('fileselect', [numFiles, label]);
-            });
-
-            $(upload_btn).on('fileselect', function (event, numFiles, label) {
-
-                var input = $(this).parents('.upload_form'),
-                    fileName = numFiles > 1 ? numFiles + ' files selected' : label;
-                if (input.length) {
-                    $('.selected-files').val("File: " + fileName);
-                }
-            });
-        });
-
-        //open mobile menu
-        $(function () {
-            var menu_btn = $('.menu_btn'),
-                side = $('.left_side_bar');
-
-                menu_btn.on('click', function () {
-                side.toggleClass('collapsed');
-            })
-        });
-
-        //set active menu item
-        $(function () {
+        activeMenuItem: function() {
             $('.nav li a').each(function (index) {
                 if (this.href.trim() == window.location)
                     $(this).addClass("active");
             });
-        });
+        },
 
-        //reset form on close modal
-        $(function(){
-            var close_md = $('.btn-close-modal');
+        openMobileMenu: function() {
+            var mobileMenu = $('.left_side_bar');
 
-                close_md.on('click', function(){
-                    $(inputs2).tooltip('destroy');
-                    $(upload_Form).trigger("reset");
-                    inputs2.removeClass('has-succes has-error');
-            });
-        });
-    });
-});
+            mobileMenu.toggleClass('collapsed');
+        }
+    };
+
+    app.initialize();
+}());
 
 
--- app/mail.php
+++ app/mail.php
@@ -1,17 +1,27 @@
 <?php
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-$captcha = $_POST['captcha'];
+session_start();
+
+// Получаем данные из формы
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$message = htmlspecialchars(strip_tags(trim($_POST['message'])), ENT_QUOTES);
 $userip = ($_SERVER['X_FORWARDED_FOR']) ? $_SERVER['X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 
+$captcha_code = $_POST['captcha'];
+$sess_captcha = $_SESSION['randStrn'];
 
 // Create the email and send the message
 $to = 'ionmnl@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."DETAILS:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n\n$message\n\nCaptcha:  $captcha\n\n"."\n=======================\nUser's IP: $userip";
 $headers = "From: noreply@metaform.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$headers .= "Reply-To: $email_address";
+
+if($sess_captcha != $captcha_code){
+    echo 'Wrong captcha';
+} else {
+    mail($to,$email_subject,$email_body,$headers);
+    echo "Your message was sent";
+}
 
 return true;
